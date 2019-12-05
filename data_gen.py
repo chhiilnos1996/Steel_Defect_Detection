91ad49cbb61e04f66ae6968a1634baf45dd776ae
ucrajkumar <urajkuma@eng.ucsd.edu>
2019-12-05 14:59:27 -0800

    Fixed train-val split

---
+++ data_gen.py
@@ -0,0 +1,78 @@
+import os
+import json
+import gc
+import cv2
+import matplotlib.pyplot as plt
+import keras
+import numpy as np
+import pandas as pd
+import sys
+from utils import build_masks
+
+class DataGenerator(keras.utils.Sequence):
+    #'Generates data for Keras'
+    def __init__(self, list_IDs, df, target_df=None, mode='fit', base_path='../input/train_images', batch_size=32, 
+                 dim=(256, 1600), n_channels=1, n_classes=4, random_state=2019, shuffle=True):
+        self.dim = dim
+        self.batch_size = batch_size
+        self.df = df
+        self.mode = mode
+        self.base_path = base_path
+        self.target_df = target_df
+        self.list_IDs = list_IDs
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.random_state = random_state     
+        self.on_epoch_end()
+
+    def __len__(self):
+        #'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        #'Generate one batch of data'
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+        # Find list of IDs
+        list_IDs_batch = [self.list_IDs[k] for k in indexes]
+        X = self.__generate_X(list_IDs_batch)
+        if self.mode == 'fit':
+            y = self.__generate_y(list_IDs_batch)
+            return X, y
+        elif self.mode == 'predict':
+            return X
+        else:
+            raise AttributeError('The mode parameter should be set to "fit" or "predict".')
+        
+    def on_epoch_end(self):
+        #'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.seed(self.random_state)
+            np.random.shuffle(self.indexes)
+    
+    def __generate_X(self, list_IDs_batch):
+        #'Generates data containing batch_size samples'
+        X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        for i, ID in enumerate(list_IDs_batch):
+            im_name = self.df['ImageId'].iloc[ID]
+            img_path = f"{self.base_path}/{im_name}"
+            img = self.__load_grayscale(img_path)
+            X[i,] = img
+        return X
+    
+    def __generate_y(self, list_IDs_batch):
+        y = np.empty((self.batch_size, *self.dim, self.n_classes), dtype=int)
+        for i, ID in enumerate(list_IDs_batch):
+            im_name = self.df['ImageId'].iloc[ID]
+            image_df = self.target_df[self.target_df['ImageId'] == im_name]
+            rles = image_df['EncodedPixels'].values
+            masks = build_masks(rles, input_shape=self.dim)
+            y[i, ] = masks
+        return y
+    
+    def __load_grayscale(self, img_path):
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        img = img.astype(np.float32) / 255.
+        img = np.expand_dims(img, axis=-1)
+        return img
--- utils.py
+++ utils.py
@@ -23,6 +23,7 @@ def BCE_loss(y_true, y_pred):
 
 def bce_dice(y_true, y_pred):
     return BCE_loss(y_true, y_pred) + dice_loss(y_true, y_pred)
+
 def post_process(probability, threshold=0.5, min_size=3000):
     '''Post processing of each predicted mask, components with lesser number of pixels
     than `min_size` are ignored'''
@@ -34,3 +35,36 @@ def post_process(probability, threshold=0.5, min_size=3000):
         if p.sum() > min_size:
             predictions[p] = 1
     return predictions
+
+
+def mask2rle(img):
+    pixels= img.T.flatten()
+    pixels = np.concatenate([[0], pixels, [0]])
+    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
+    runs[1::2] -= runs[::2]
+    return ' '.join(str(x) for x in runs)
+
+def rle2mask(rle, input_shape):
+    width, height = input_shape[:2]
+    mask= np.zeros( width*height ).astype(np.uint8)
+    array = np.asarray([int(x) for x in rle.split()])
+    starts = array[0::2]
+    lengths = array[1::2]
+    current_position = 0
+    for index, start in enumerate(starts):
+        mask[int(start):int(start+lengths[index])] = 1
+        current_position += lengths[index]
+    return mask.reshape(height, width).T
+
+def build_masks(rles, input_shape):
+    depth = len(rles)
+    masks = np.zeros((*input_shape, depth))
+    for i, rle in enumerate(rles):
+        if type(rle) is str:
+            masks[:, :, i] = rle2mask(rle, input_shape)
+    return masks
+
+def build_rles(masks):
+    width, height, depth = masks.shape
+    rles = [mask2rle(post_process(masks[:, :, i])) for i in range(depth)]
+    return rles
